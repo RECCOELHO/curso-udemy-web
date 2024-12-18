2dab4d16d7d7ef7013bb404dfd7a8b9d853a0538
Carlos Jessé Coelho <63757384+RECCOELHO@users.noreply.github.com>
2024-12-18 08:54:06 -0300

    aquivo de alguem.

---
+++ estudo_python/index.py
@@ -0,0 +1,73 @@
+#1)
+#a = "Joao"
+#b = "Carlos"
+#c = "Maria"
+
+#print(a, b, c)
+
+#2)
+
+#x = 8
+
+#if x < 10:
+   # print("A variavel e menor do que 10!!")
+#else:
+ #   print("A variavel e maior ou igual a 10!!")
+
+#3)
+
+#x = 30
+
+#if x <= 10:
+   # print("A variavel e menor ou igual a 10")
+#elif x > 10 and x <= 25:
+ #   print("A variavel e maior do que 10 e menor ou igual a 25")
+#else:
+ #   print("A variavel e maior do que 25")
+
+
+#4)
+#a = 150
+#b = 10
+
+
+#divide_variaveis = a / b 
+#print(divide_variaveis % 2)
+
+
+#5)
+
+#var1 = int(input("Digite um numero: "))
+#var2 = int(input("Digite outro numero"))
+
+
+#divide_variavel = var1 / var2
+
+#print(divide_variavel)
+
+#6)
+
+# age = int(input("Digite um inteiro: "))
+
+
+# if age == 20:
+#     print("Joao")
+# elif age == 10:
+#     print("Maciel")
+# elif age == 30:
+#     print("Maria")
+# else:
+#     print("Pedro")
+
+#Funções
+
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+    
+soma = a + b
+diferenca = a - b
+produto = a * b
+    
+print(soma)
+print(diferenca)
+print(produto)
